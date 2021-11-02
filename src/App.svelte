1a5684ceebe9c93c608158d77d90a6293e7fd96f
LollerCoder <jrjaropojop@evc.pshs.edu.ph>
2021-11-02 15:58:06 +0700

    Bootstrap Update
    Made it to be the same as output given to sir.

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,7 @@
 <svelte:head>
+  <title>Sportsfest Scheduler</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </svelte:head>
 
@@ -38,67 +41,126 @@
   }
 </script>
 
+
 <body>
   <div>
-    <div id="aside"></div>
-    <div id="form-1">
-      <div class="form-group">
-        <label for="n_days">No. of days for Sportsfest</label>
-        <input type="number" class="form-control" id="n_days" placeholder="1" bind:value={$sf.days}/>
+    <div class="row me-0 ">
+      <div class="intro col-sm-12 col-md-4 pb-3">
+         <p class="mb-0 px-3 py-5 ">The SF Scheduler is an SIP Project created by Julyanna Huang, Jairo Jaropojop 
+          and Adrian Mayores with the goal of creating an algorithm that would streamline the process 
+          of Sportsfest Schedule making. 
+          <br><br>
+          Please fill out the necessary data and the Computer will do the rest.</p>
+        
+      </div>
+    
+      <div class="forms col-sm-12 col-md-4 pb-3">
+        <p>Sportsfest Settings</p>
+        <div class="form-group">
+          <label for="n_days">No. of days for Sportsfest</label>
+          <input type="number" class="form-control" id="n_days" placeholder="1" bind:value={$sf.days}/>
+        </div>
+
+        <div class="form-group">
+          <label for="n_timeslots_per_day">Number of Timeslots Per Day</label>
+          <input type="number" class="form-control" id="n_timeslots_per_day" placeholder="1" bind:value={$sf.n_timeslots_per_day}/>
+        </div>
+        <div class="form-group">
+          <label for="timeslot_length">Length of Timeslot in Sportsfest</label>
+          <input type="number" class="form-control" id="timeslot_length" placeholder="1" bind:value={$sf.timeslot_length}/>
+        </div>
       </div>
 
-      <div class="form-group">
-        <label for="n_timeslots_per_day">Number of Timeslots Per Day</label>
-        <input type="number" class="form-control" id="n_timeslots_per_day" placeholder="1" bind:value={$sf.n_timeslots_per_day}/>
-      </div>
-      <div class="form-group">
-        <label for="timeslot_length">Length of Timeslot in Sportsfest</label>
-        <input type="number" class="form-control" id="timeslot_length" placeholder="1" bind:value={$sf.timeslot_length}/>
-      </div>
-    </div>
-    <div id="form-2">
-      <div class="form-group">
-        <label for="game-selection">Game Selection</label>
-        <select class="form-control" id="game-selection" bind:value={current_game}>
-          {#each game_names as g}
-            <option>{g}</option>
-          {/each}
-          <option>New Game</option>
-        </select>
-      </div>
+      <div class="forms col-sm-12 col-md-4 pb-3">
+        <p>Game Settings(WIP)</p>
+        <div class="form-group">
+          <label for="game-selection">Game Selection</label>
+          <select class="form-control" id="game-selection" bind:value={current_game}>
+            {#each game_names as g}
+              <option>{g}</option>
+            {/each}
+            <option>New Game</option>
+          </select>
+        </div>
 
-      <div class="form-group">
-        <label for="game-name">Name</label>
-        <input type="text" class="form-control" id="game-name" bind:value={current_game_name}/>
-      </div>
+        <div class="form-group">
+          <label for="game-name">Name</label>
+          <input type="text" class="form-control" id="game-name" bind:value={current_game_name}/>
+        </div>
 
-      <div class="form-group">
-        <label for="n_timeslots_per_round">Number of Timeslots Per Rounds</label>
-        <input type="number" class="form-control" id="n_timeslots_per_round" bind:value={current_timeslots_per_round}/>
-      </div>
+        <div class="form-group">
+          <label for="n_timeslots_per_round">Number of Timeslots Per Rounds</label>
+          <input type="number" class="form-control" id="n_timeslots_per_round" bind:value={current_timeslots_per_round}/>
+        </div>
 
-      <div class="form-group">
-        <label for="priority">Priority</label>
-        <select class="form-control" bind_value={current_priority}>
-          <option>Major</option>
-          <option>Minor</option>
-          <option>Junior</option>
-        </select>
-      </div>
+        <div class="form-group">
+          <label for="priority">Priority</label>
+          <select class="form-control" bind_value={current_priority}>
+            <option>Major</option>
+            <option>Minor</option>
+            <option>Junior</option>
+          </select>
+        </div>
 
-      <div class="form-group">
-        <label for="categories">Number of categories</label>
-        <input type="number" class="form-control" id="categories" bind:value={current_categories}/>
-      </div>
+        <div class="form-group">
+          <label for="categories">Number of categories</label>
+          <input type="number" class="form-control" id="categories" bind:value={current_categories}/>
+        </div>
+      
 
-      <button class="btn btn-primary" on:click={addGame}>Add game</button>
+      <button class="btn custom-color" on:click={addGame}>Add game</button>
     </div>
   </div>
-  <button class="btn btn-primary" on:click={submitData}>Submit data</button>
+  <div class="results ">
+    <p class="col-12mb-0 px-3 pt-3 pb-1 ">Did you already Input everything that you need? If so, please fill in your email below. The algorithm takes 
+      usually 2 hours to create a result. To make it convenient for you, it'll email you the result once it has been created. After filling 
+      in your email, Click the button to create a schedule and wait for the results to be emailed.</p>
+    <form class="col-8 offset-2 pb-3">
+      <div class>
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      </div>
+      <button class="btn custom-color" on:click={submitData}>Submit data</button>
+    </form>  
+  </div>
+  <footer>
+    <span class=" d-block p-2 text-white ">Developed by Huang, J., Jaropojop, J., Mayores, A.
+      <br> Copyright &copy;  2021 PSHS-EVC All rights reserved </span>
+    
+  </footer>
+  
 </body>
 
 <style>
-  div {
-    margin: 1em;
+  footer {
+    color:#FCF7FF;
+    text-align:center;
+    background-color: #0C0910;
+    padding: 0.8em;
+    
+  }
+  .intro{
+    background-color:#373F47;
+    color:#F5F5F5;
+    
+  }
+  .forms{
+    background-color:#F5F5F5;
+  }
+  .results{
+    background-color: #2E82DC;
+    color: #F5F5F5;
+  }
+  h3{
+    text-align: center;
+  }
+  #emailHelp{
+    color: #F5F5F5;
+  }
+  button.custom-color{
+    background-color: #F24F40;
+    color: #F5F5F5;
   }
 </style>
+
