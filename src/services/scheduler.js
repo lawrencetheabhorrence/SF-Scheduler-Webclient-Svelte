b4e861684c460bf98bf7c0f63c4e13cf02f6effc
lawrencetheabhorrence <luluemilianolawrence@gmail.com>
2021-11-26 12:25:31 +0800

    minor backend url change

--- src/services/scheduler.js
+++ src/services/scheduler.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://sf-scheduler-algo.herokuapp.com/'
+const baseUrl = 'https://sf-scheduler-algo.herokuapp.com'
 
 export const sendData = (data) => {
   axios.post(`${baseUrl}/scheduler/`, JSON.stringify(data))
