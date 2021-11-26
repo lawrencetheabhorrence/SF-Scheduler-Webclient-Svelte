1e5925dc5f9b90d5fbebcfc6492de55e6a5f6655
lawrencetheabhorrence <luluemilianolawrence@gmail.com>
2021-11-26 15:07:46 +0800

    slash appended to url

--- src/services/scheduler.js
+++ src/services/scheduler.js
@@ -3,5 +3,5 @@ const baseUrl = 'https://sf-scheduler-algo.herokuapp.com'
 //const baseUrl = "http://127.0.0.1:8000"
 
 export const sendData = (data) => {
-  axios.post(`${baseUrl}/scheduler`, JSON.stringify(data))
+  axios.post(`${baseUrl}/scheduler/`, JSON.stringify(data))
 }
