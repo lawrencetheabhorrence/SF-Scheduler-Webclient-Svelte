9d2bc73cbf1e6b5fe843bc138c3f26459e478848
lawrencetheabhorrence <luluemilianolawrence@gmail.com>
2022-02-28 20:44:38 +0800

    fixed backend link

--- src/services/scheduler.js
+++ src/services/scheduler.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 //const baseUrl = 'https://sf-scheduler-algo.herokuapp.com' // server backend
-const baseUrl = "http://127.0.0.1:8000" // local host
-//const baseUrl = "https://young-cove-10921.herokuapp.com"
+//const baseUrl = "http://127.0.0.1:8000" // local host
+const baseUrl = "https://young-cove-10921.herokuapp.com"
 
 export const sendData = (data) => {
   axios.post(`${baseUrl}/webclient/`, JSON.stringify(data))
